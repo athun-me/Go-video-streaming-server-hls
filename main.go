5cbe0ef5b6aae3a1a63a14297cdfb13b8b7c353e
Athun lal <athunlalp#gmail.com>
2023-06-22 12:50:12 +0530

    main.go file updated

--- main.go
+++ main.go
@@ -7,17 +7,21 @@ import (
 )
 
 func main() {
+	// configure the songs directory name and port
 	const songsDir = "songs"
-	const port = 8000
+	const port = 8080
 
-	http.Handle("/", addHeader(http.FileServer(http.Dir(songsDir))))
+	// add a handler for the song files
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(songsDir))))
 	fmt.Printf("Starting server on %v\n", port)
-	log.Printf("Serving %s on HTTP port :%v\n", songsDir, port)
+	log.Printf("Serving %s on HTTP port: %v\n", songsDir, port)
 
+	// serve and log errors
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func addHeader(h http.Handler) http.HandlerFunc {
+// addHeaders will act as middleware to give us CORS support
+func addHeaders(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		h.ServeHTTP(w, r)
